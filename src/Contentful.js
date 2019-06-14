60c4211c9a53e5bf856c07d0d83324c802966b0e
Ramanuja Sreenidhi <rsreenidhi@hawk.iit.edu>
2019-06-13 21:07:50 -0700

    Final Project Commit

---
+++ src/Contentful.js
@@ -0,0 +1,6 @@
+import {createClient} from 'contentful';
+
+export default createClient({
+    space: "0koeeoe5xz1b",
+    accessToken: "kWrDiX6AAd7Ho5KxDSwntGvCf-eWx6_dptjh09ArezY"
+});
--- src/context.js
+++ src/context.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import items from './data';
+//import items from './data';
+import Client from './Contentful';
+
+
+
 const RoomContext = React.createContext();
 
 
@@ -21,23 +25,35 @@ class RoomProvider extends React.Component {
     };
 
     //getData
-
+    getData = async () => {
+        try{
+            let response = await Client.getEntries(
+                {
+                    content_type: "beacResortRoom",
+                    //order: "sys.createdAt"
+                    order: 'fields.price'
+                });
+            let rooms = this.formatData(response.items);
+            let featuredRooms = rooms.filter(room => {
+                return room.featured === true;
+            });
+            let maxPrice = Math.max(...rooms.map(item => item.price));
+            let maxSize = Math.max(...rooms.map(item => item.size));
+            this.setState({
+                rooms,
+                featuredRooms,
+                sortedRooms: rooms,
+                loading: false,
+                price: maxPrice,
+                maxPrice,
+                maxSize
+            });
+        }catch (error){
+            console.log(error);
+        }
+    }
     componentDidMount(){
-        let rooms = this.formatData(items);
-        let featuredRooms = rooms.filter(room => {
-            return room.featured === true;
-        });
-        let maxPrice = Math.max(...rooms.map(item => item.price));
-        let maxSize = Math.max(...rooms.map(item => item.size));
-        this.setState({
-            rooms,
-            featuredRooms,
-            sortedRooms: rooms,
-            loading: false,
-            price: maxPrice,
-            maxPrice,
-            maxSize
-        });
+        this.getData();
     }
 
     formatData(items){
